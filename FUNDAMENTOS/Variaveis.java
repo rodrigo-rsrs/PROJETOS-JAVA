da849066179c4847dbce92b9dd11eda90d6f7dfe
Rodrigo Jesus Sena dos Santos <rjssantos.2007@gmail.com>
2026-09-10 06:02:02 -0300

    Treino de variaveis

---
+++ FUNDAMENTOS/Variaveis.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String nome = "Rodrigo";
+        int idade = 19;
+        double altura = 1.87;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+    }
+}
